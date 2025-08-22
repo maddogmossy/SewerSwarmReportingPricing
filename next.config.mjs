462b1791d6093e3b289b0ee14cb3b0dfed550f64
maddogmossy <maddogmossy@me.com>
2025-08-22 08:25:56 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { experimental: { appDir: true } };
+const nextConfig = {
+  experimental: { appDir: true },
+};
+
 export default nextConfig;
