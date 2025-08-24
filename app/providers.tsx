839edcc92f75a38522783fd00a46d2c9914945f2
maddogmossy <maddogmossy@me.com>
2025-08-24 15:13:51 +0800

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,13 @@
+// app/providers.tsx
 "use client";
 
+import * as React from "react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <TooltipProvider>
+    <TooltipProvider delayDuration={150}>
       {children}
       <Toaster />
     </TooltipProvider>
