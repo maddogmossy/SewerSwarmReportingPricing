dca53d5ea08a2dcd8eb7b4edc6ddc7e79719baae
maddogmossy <maddogmossy@me.com>
2025-08-24 18:07:05 +0800

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,12 +3,14 @@
 
 import * as React from "react";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { Toaster } from "@/components/ui/toaster";
+// ⬇⬇ default import (not named) fixes the build error
+import Toaster from "@/components/ui/toaster";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <TooltipProvider delayDuration={150}>
+    <TooltipProvider delayDuration={200}>
       {children}
+      {/* global toast portal */}
       <Toaster />
     </TooltipProvider>
   );
