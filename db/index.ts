fbde2f70543883a14e7e764e46433356811aaa7b
maddogmossy <maddogmossy@me.com>
2025-08-27 10:29:10 +0800

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,10 @@
-// /db/index.ts
+// db/index.ts
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
-const connectionString = process.env.DATABASE_URL!;
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not set");
-}
-
 const pool = new Pool({
-  connectionString,
-  ssl: { rejectUnauthorized: false }, // Neon
+  connectionString: process.env.DATABASE_URL, // Neon connection string
+  ssl: { rejectUnauthorized: false },
 });
 
 export const db = drizzle(pool);
